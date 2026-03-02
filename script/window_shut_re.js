5b0ec5d615cf3eee390b16aa0e063d3845a4e8d7
O3-O2O <hunghrbest@gmail.com>
2026-03-02 19:08:38 +0700

    Add files via upload

--- script/window_shut_re.js
+++ script/window_shut_re.js
@@ -2,6 +2,8 @@ window.funcBtn = (called) => {
     window.location.href = `window_shut_re.html?mode=${called}`;
 }
 
+const shutdowning = new Audio("../audio/shutdown_xp.mp3")
+
 window.addEventListener("DOMContentLoaded", () => {
 
     const params = new URLSearchParams(window.location.search);
@@ -13,11 +15,20 @@ window.addEventListener("DOMContentLoaded", () => {
 
     if (mode === "Shutdown") {
         message.innerHTML = "Windows is shutting down...";
+
+        setTimeout(() => {
+
+            shutdowning.play()
+
+        },5000)
     } else if (mode === "Restart") {
         message.innerHTML = "Windows is restarting...";
     }
 
+    
+
     setTimeout(() => {
+        
         document.getElementById("box").style.opacity = "0";
         document.body.classList.add("black-screen");
 
